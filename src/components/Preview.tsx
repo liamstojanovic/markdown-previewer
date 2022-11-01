2d30440bcd1ff33e50af11c64a88aaba30394162
Liam <liam.stojanovic@gmail.com>
2022-11-01 12:26:51 -0400

    styles

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -3,15 +3,22 @@ import { PreviewState } from "./Editor";
 import { marked } from "marked";
 import DOMPurify from "dompurify";
 
+// Leveraged https://github.com/sindresorhus/github-markdown-css for Markdown styles.
+import './Preview.css';
+import './Preview_Dark.css'
+
 const Preview = (props:PreviewState) => {
     // Convert markdown to HTML
-    const rawHtml = marked.parse(props.raw);
+    const rawHtml = marked.parse(props.raw, {breaks: true});
 
     // Sanitize HTML prior to rendering to DOM
     const sanitizedHtml = DOMPurify.sanitize(rawHtml);
+    
     return (
-        <div id="preview" dangerouslySetInnerHTML={{ __html: sanitizedHtml }}>
-            
+        <div 
+            id="preview" 
+            className="markdown-body text-white border border-white m-2" 
+            dangerouslySetInnerHTML={{ __html: sanitizedHtml }}>
         </div>
     )
 };
